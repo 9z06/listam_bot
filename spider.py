46cea29dcbb4ebe730c7258c4a2eb565a0bc15a3
9z06 <posmotrivlevo@gmail.com>
2022-08-06 20:13:12 +0300

    change formatting

--- spider.py
+++ spider.py
@@ -1,10 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.list.am/category/63'
+url = "https://www.list.am/category/63"
 
 page = requests.get(url)
 
 soup = BeautifulSoup(page.text, "html.parser")
-hatka_all = soup.find_all('div', {'class': 'gl'})
-
+all_urls = soup.find_all("div", {"class": "gl"})
